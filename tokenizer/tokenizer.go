51cffe95aba0cd1c23cb87fdd9d30abc86a8c9c0
ARF-DEV <ariefuddinsatriadharma@gmail.com>
2026-08-23 20:15:38 +0700

    init: isAlpha && isWhitespace

---
+++ tokenizer/tokenizer.go
@@ -0,0 +1,44 @@
+package tokenizer
+
+// Tokenizer follows Requirements For The SQLite Tokenizer
+
+type charClass byte
+
+const (
+	WHITESPACE   charClass = 0
+	ALPHABETIC   charClass = 1
+	NUMERIC      charClass = 2
+	ALPHANUMERIC charClass = 3
+	HEXADECIMAL  charClass = 4
+	SPECIAL      charClass = 5
+)
+
+// One of these five characters: u0009, u000a, u000c, u000d, or u0020
+func isWhiteSpace(character rune) bool {
+	if character == 0x009 ||
+		character == 0x000a ||
+		character == 0x000c ||
+		character == 0x000d ||
+		character == 0x0020 {
+		return true
+	}
+	return false
+}
+
+// Any of the characters in the range u0041 through u005a (letters "A" through "Z")
+// or in the range u0061 through u007a (letters "a" through "z")
+// or the character u005f ("_") or any other character larger than u007f.
+func isAlphabetic(character rune) bool {
+	if character >= 'A' && character <= 'Z' ||
+		character >= 'a' && character <= 'z' ||
+		character == '_' ||
+		character > 0x007f {
+		return true
+	}
+	return false
+}
+
+// type tokenizer struct{}
+//
+// func (t *tokenizer)  {
+// }
---
+++ tokenizer/tokenizer_test.go
@@ -0,0 +1,33 @@
+package tokenizer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsWhiteSpace(t *testing.T) {
+	t.Run("Is a whitespace", func(t *testing.T) {
+		assert.Equal(t, true, isWhiteSpace(' '))
+		assert.Equal(t, true, isWhiteSpace(0x0020))
+		assert.Equal(t, true, isWhiteSpace(0x0009))
+		assert.Equal(t, true, isWhiteSpace(0x000a))
+		assert.Equal(t, true, isWhiteSpace(0x000c))
+		assert.Equal(t, true, isWhiteSpace(0x000d))
+	})
+	t.Run("Not a whitespace", func(t *testing.T) {
+		assert.Equal(t, false, isWhiteSpace('A'))
+	})
+}
+
+func TestIsAlphabetic(t *testing.T) {
+	t.Run("Is an alphabet", func(t *testing.T) {
+		assert.Equal(t, true, isAlphabetic('A'))
+		assert.Equal(t, true, isAlphabetic('a'))
+		assert.Equal(t, true, isAlphabetic('_'))
+		assert.Equal(t, true, isAlphabetic('_'))
+	})
+	t.Run("Is not an alphabet", func(t *testing.T) {
+		assert.Equal(t, false, isAlphabetic('{'))
+	})
+}
